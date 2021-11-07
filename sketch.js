d99deacc98619386da315c3c652bb29b8e50c108
sidak-245 <74825336+sidak-245@users.noreply.github.com>
2021-11-07 22:42:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var cross;
+var circle;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
